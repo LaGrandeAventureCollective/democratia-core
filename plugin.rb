e8e3eb7373564c56a67802b2ece7770b44cda4c2
Samuel <samuel200182@gmail.com>
2026-07-11 22:08:31 +0000

    Register onboarding completion user field

--- plugin.rb
+++ plugin.rb
@@ -10,6 +10,7 @@ enabled_site_setting :democratia_core_enabled
 require_relative "lib/democratia_core/engine"
 
 DEMOCRATIA_ONBOARDING_COMPLETED_FIELD = "democratia_onboarding_completed"
+register_user_custom_field_type(DEMOCRATIA_ONBOARDING_COMPLETED_FIELD, :boolean)
 
 
 after_initialize do
